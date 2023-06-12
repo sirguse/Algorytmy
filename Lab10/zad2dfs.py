1c04da0d307bf48866932a83639203310177cde7
sirguse <115590503+sirguse@users.noreply.github.com>
2023-06-12 15:24:26 +0200

    Create zad2dfs.py

---
+++ Lab10/zad2dfs.py
@@ -0,0 +1,24 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+
+    visited.add(start)
+
+    print(start)
+
+    for next_node in graph[start] - visited:
+        dfs(graph, next_node, visited)
+
+    return visited
+
+# Przykładowy graf do przetestowania
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'},
+}
+
+dfs(graph, 'A')
